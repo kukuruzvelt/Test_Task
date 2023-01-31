a8ca5c496e6db62b54318304e172bd7a4f4a33d2
kukuruzvelt <81823080+kukuruzvelt@users.noreply.github.com>
2023-01-31 11:19:40 +0200

    Initial commit

---
+++ src/main/java/model/impl/Laptop.java
@@ -0,0 +1,7 @@
+package model.impl;
+
+import model.Computer;
+
+public class Laptop extends Computer {
+
+}
---
+++ src/main/java/model/impl/PersonalComputer.java
@@ -0,0 +1,7 @@
+package model.impl;
+
+import model.Computer;
+
+public class PersonalComputer extends Computer {
+
+}
